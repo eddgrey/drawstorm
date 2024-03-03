580384b08f1a71e3ee11b38ad636842e874051b7
Eduardo Garcia <peduardogarcia.dev@gmail.com>
2024-03-02 19:33:07 -0600

    feat: add dropdown menu to user button

--- src/app/dashboard/_components/user-button.tsx
+++ src/app/dashboard/_components/user-button.tsx
@@ -1,10 +1,44 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Hint } from "@/components/ui/hint";
+import { useRouter } from "next/navigation";
 
 export default function UserButton() {
+  const router = useRouter();
+
+  const handleSignOut = () => {
+    router.push("/");
+  };
+
   return (
-    <Avatar>
-      <AvatarImage src="https://github.com/shadcn.png" />
-      <AvatarFallback>CN</AvatarFallback>
-    </Avatar>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Settings</DropdownMenuItem>
+        <DropdownMenuItem className="focus:bg-transparent">
+          <Button size="sm" className="w-full" onClick={handleSignOut}>
+            Sign out
+          </Button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
