2f35ac072facdb597f42280368432b089d417d36
Eduardo Garcia <peduardogarcia.dev@gmail.com>
2024-03-05 16:47:04 -0600

    feat: display participants in the room

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,6 +4,8 @@ import { v4 as uuidv4 } from "uuid";
 
 import { boardImages } from "@/data";
 
+const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -15,4 +17,8 @@ export function getRandomBoardImage() {
 
 export function getRandomId() {
   return uuidv4();
+}
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
 }
